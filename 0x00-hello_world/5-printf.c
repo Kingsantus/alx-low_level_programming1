e76bf4f0c637ad008f196dca407db8347ccdeba4
kingsantus <nkingsantus@gmail.com>
2023-02-17 20:21:13 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main = entry point
+ * 
+ * return = 0
+ */
+int main() {
+   printf("with proper grammar, but the outcome is a piece of art,\n");
+   return 0;
+}
