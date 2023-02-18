5d767b1656c2c8f630b0b86b0e6f6d4f0d4e2288
kingsantus <nkingsantus@gmail.com>
2023-02-17 20:11:38 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main = entry point
+ *
+ * Return: is 0
+ */
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
