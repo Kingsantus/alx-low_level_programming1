90485cf046de296691d8384de6d223d5fb36cb81
kingsantus <nkingsantus@gmail.com>
2023-02-18 21:42:06 -0800

    both upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print in lowcase and uppercase
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'A'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
