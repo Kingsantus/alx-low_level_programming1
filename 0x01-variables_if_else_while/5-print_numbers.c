f904195db6044231682da992021b85ae3affbac5
kingsantus <nkingsantus@gmail.com>
2023-02-18 22:26:34 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for code
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf('%d\n', i);
+	}
+	return(0);
+}
