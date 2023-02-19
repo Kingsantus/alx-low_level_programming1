c7c9ca22316a7c0866a4fe6fe3225b90ea825abc
kingsantus <nkingsantus@gmail.com>
2023-02-19 05:06:38 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	int num;
 	char le;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num <= 9; num++)
 		putchar((num % 10) + '0');
 	for (le = 'a'; le <= 'f'; le++)
 		putchar(le);
