d8007bf7f25ee33bcfe3c3f78d0a58ddd36b5240
kingsantus <nkingsantus@gmail.com>
2023-02-19 01:28:57 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for data
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
