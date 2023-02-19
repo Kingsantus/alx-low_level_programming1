e8e061a23cd75325054ea9d8c4f202ae58248843
kingsantus <nkingsantus@gmail.com>
2023-02-19 08:40:13 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,38 +7,21 @@
  */
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
+	int num1;
+	int num2;
 
-	int c2;
-	int f_d2;
-	int l_d2;
-
-	while (c <= 98)
+	for (num1 = 0; num1 < 100; num1++)
 	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-		while (c2 < 98)
+		for (num2 = num1 + 1; num2 < 100; num2++)
 		{
-			f_d2 = (c2 / 10 + '0');
-			l_d2 = (c2 % 10 + '0');
-
-			if (c < c2)
-			{
-				putchar(f_d);
-				putchar(l_d);
-				putchar(' ');
-				putchar(f_d2);
-				putchar(l_d2);
-
-				if (c != 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			} c2++;
-		} c++;
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
