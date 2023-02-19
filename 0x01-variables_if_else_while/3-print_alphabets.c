b2ac62e68b202cbc67188a96a9f223108c0a0b46
kingsantus <nkingsantus@gmail.com>
2023-02-18 21:45:30 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,11 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	{
 		putchar(letter);
-	}
+
 	for (letter = 'A'; letter <= 'A'; letter++)
-	{
 		putchar(letter);
-	}
+
 	putchar('\n');
 	return (0);
 }
