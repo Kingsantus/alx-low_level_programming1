e66169c435417195629eb300d02640b477915597
kingsantus <nkingsantus@gmail.com>
2023-02-22 14:31:53 -0800

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @n1: the first number to be completed
+ * @n2: the second number to be completed
+ * Return: Integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
