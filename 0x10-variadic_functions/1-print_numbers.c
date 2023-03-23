16c225a2706400b097bc3aafddcf01cfef5d1300
kingsantus <nkingsantus@gmail.com>
2023-03-23 13:24:15 -0700

    betty handle

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (index = 0; index < n; index++)
 	{
 		printf("%d", va_arg(nums, int));
-		
+
 		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
