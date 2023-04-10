48b8460ba98801e949db6aac024c2f3b5f9f6f4f
kingsantus <nkingsantus@gmail.com>
2023-04-10 16:04:16 -0700

    0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -22,18 +22,11 @@ unsigned int binary_to_uint(const char *b)
 	}
 	for (int i = 0; b[i] != '0'; i++)
 	{
-		if (b[i] == '0')
-		{
-			result = result << 1;
-		}
-		else if (b[i] == '1')
-		{
-			result = (result << 1) | 1;
-		}
-		else
+		if (b[i] <  '0' || b[i] > '1')
 		{
 			return (0);
 		}
+		result = 2 * result + (b[i] - '0');
 	}
 	return (result);
 }
