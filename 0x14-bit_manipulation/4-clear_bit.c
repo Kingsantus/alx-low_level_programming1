c93bcc275097b3c73334609e1ea1a1b570a65ef8
kingsantus <nkingsantus@gmail.com>
2023-04-11 14:51:35 -0700

    100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
+ *
+ * Return: value of the bit
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	*n &= ~(1UL << index);
+	return (1);
+}
