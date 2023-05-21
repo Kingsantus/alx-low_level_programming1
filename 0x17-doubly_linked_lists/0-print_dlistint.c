7ce3fb9ad4e3d39aebe8e416b2f851f4b4c1e9e9
kingsantus <nkingsantus@gmail.com>
2023-05-21 06:25:24 -0700

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,17 +8,17 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *ptr = h;
+	const dlistint_t *temp = h;
 	size_t count = 0;
 
 	if (!h)
 		return (count);
 
-	while (ptr != NULL)
+	while (!temp)
 	{
-		printf("%d \n", ptr->n);
+		printf("%d\n", temp->n);
 		count++;
-		ptr = ptr->next;
+		temp = temp->next;
 	}
 
 	return (count);
